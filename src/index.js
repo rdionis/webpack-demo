013fe1b5a6f7432d40c14525aba453c162357b79
Raquel Dionísio <dionisio@stadtwerk.org>
2022-06-28 10:46:24 +0200

    installed and added lodash

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import _ from "lodash";
+
 const component = () => {
     const element = document.createElement("div");
- 
+    
+    // Lodash, now imported by this script
     element.innerHTML = _.join(["Hello", "webpack"], "");
 
     return element
